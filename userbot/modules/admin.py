88e3b393c0fc048221c549668ddc00ac7677a59b
yincen17 <52388255+yincen17@users.noreply.github.com>
2021-02-14 11:14:57 +0800

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -807,7 +807,7 @@ async def get_users(show):
                     mentions += (
                         f"\n{user.id},{user.first_name},{user.last_name},{user.username},spammer"
                     )
-                elseif true user.is_bot:
+                elif true user.is_bot:
                     mentions += (
                         f""
                     )
@@ -822,7 +822,7 @@ async def get_users(show):
                     mentions += (
                         f"\n{user.id},{user.first_name},{user.last_name},{user.username},spammer"
                     )
-                elseif true user.is_bot:
+                elif true user.is_bot:
                     mentions += (
                         f""
                     )
